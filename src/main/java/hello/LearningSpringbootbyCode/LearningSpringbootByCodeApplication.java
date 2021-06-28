c2e3874c4040d883d90b8b939d0b0926ce7f421e
CH0213 <cndghks0213@gmail.com>
2021-06-28 16:50:08 +0900

    프로젝트 생성

---
+++ src/main/java/hello/LearningSpringbootbyCode/LearningSpringbootByCodeApplication.java
@@ -0,0 +1,13 @@
+package hello.LearningSpringbootbyCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringbootByCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringbootByCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/LearningSpringbootbyCode/LearningSpringbootByCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.LearningSpringbootbyCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringbootByCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

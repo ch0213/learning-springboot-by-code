f8e39b44d1d02ded90cc71369a6a50eeec4f9637
CH0213 <cndghks0213@gmail.com>
2021-07-01 23:50:44 +0900

    test: 회원가입() 구현

--- src/test/java/hello/LearningSpringbootbyCode/service/MemberServiceTest.java
+++ src/test/java/hello/LearningSpringbootbyCode/service/MemberServiceTest.java
@@ -1,13 +1,30 @@
 package hello.LearningSpringbootbyCode.service;
 
+import hello.LearningSpringbootbyCode.domain.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
+    MemberService memberService = new MemberService();
+
     @Test
     void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
     }
 
     @Test

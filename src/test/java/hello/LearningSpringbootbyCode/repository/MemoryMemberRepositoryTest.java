55baa811bad9da3ca8b88af989e3fb21800908fa
CH0213 <cndghks0213@gmail.com>
2021-07-01 16:03:30 +0900

    test: findAll() 추가

--- src/test/java/hello/LearningSpringbootbyCode/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/LearningSpringbootbyCode/repository/MemoryMemberRepositoryTest.java
@@ -4,6 +4,7 @@ import hello.LearningSpringbootbyCode.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -23,12 +24,12 @@ class MemoryMemberRepositoryTest {
     }
 
     @Test
-    public void findbyName() {
+    public void findByName() {
         //given
         Member member1 = new Member();
         member1.setName("spring1");
         repository.save(member1);
-        
+
         Member member2 = new Member();
         member2.setName("spring2");
         repository.save(member2);
@@ -38,5 +39,20 @@ class MemoryMemberRepositoryTest {
 
         //then
         assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
     }
 }

e5b8c20570d3b2358803b91740a2eac8263dbcf4
CH0213 <cndghks0213@gmail.com>
2021-07-01 15:54:02 +0900

    test: findbyName 테스트 추가

--- src/test/java/hello/LearningSpringbootbyCode/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/LearningSpringbootbyCode/repository/MemoryMemberRepositoryTest.java
@@ -4,6 +4,8 @@ import hello.LearningSpringbootbyCode.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.*;
 
 class MemoryMemberRepositoryTest {
@@ -18,5 +20,22 @@ class MemoryMemberRepositoryTest {
 
         Member result = repository.findbyId(member.getId()).get();
         assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findbyName() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        //when
+        Member result = repository.findbyName("spring1").get();
+
+        //then
+        assertThat(result).isEqualTo(member1);
     }
 }

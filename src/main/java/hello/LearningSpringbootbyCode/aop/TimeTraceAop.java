656e22c859cd6aceed706cbc9c91c94ccf28d0d0
CH0213 <cndghks0213@gmail.com>
2021-07-16 22:56:19 +0900

    feat: AOP로 시간 측정 기능 구현

---
+++ src/main/java/hello/LearningSpringbootbyCode/aop/TimeTraceAop.java
@@ -0,0 +1,27 @@
+package hello.LearningSpringbootbyCode.aop;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class TimeTraceAop {
+
+    @Around("execution(* hello.LearningSpringbootbyCode..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long start = System.currentTimeMillis();
+
+        System.out.println("START: " + joinPoint.toString());
+
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+
+            System.out.println("END: " + joinPoint.toString()+ " " + timeMs + "ms");
+        }
+    }
+}
